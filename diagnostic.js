d0875791b2af22e586044c7a3a731a922f2feafd
Mikael Krantz <work-mk@mk.se>
2020-07-14 12:31:28 +0200

    adding iframe (rediscovering cors :) and browser agent

--- diagnostic.js
+++ diagnostic.js
@@ -35,14 +35,24 @@ function probeUrlAsync(probeUrl)
 }
 
 var probeUrls = [
-                 {"id":"naive_test_1", "note":"static http request test 1", "url":"http://mk.se/", "expectStatus":[200], "timeout":30000},
+                 {"id":"naive_test_1", "note":"static http request test 1", "url":"https://mk.se/", "expectStatus":[200], "timeout":30000},
                  {"id":"e_identity", "note":"E-identitet IDP", "url":"https://login.grandid.com/", "expectStatus":[401], "timeout":30000},
-]
+                 ];
 
-var listHtml = "<dl>"
+var iframeUrls = [
+                  {"id":"naive_iframe", "note":"simple iframe test", "url":"http://mk.se/", "width":"100%", "height":"50"},
+                  {"id":"frame_e_identity", "note":"E-identitet IDP (expected to say \"Unauthorized\")", "url":"https://login.grandid.com/", "width":"100%", "height":"50"},
+                  ];
+
+var listHtml = "<dl>";
+listHtml += '<dt> Browser agent</dt><dd>'+navigator.userAgent+'</dd>';
 for (var i=0; i < probeUrls.length; i++) {
   var probeUrl = probeUrls[i];
   listHtml += "<dt>" + probeUrl.note + " [" + probeUrl.url + ']</dt><dd id="'+ probeUrl.id + '">checking...</dd>';
+}
+for (var i=0; i < iframeUrls.length; i++) {
+  var iframeUrl = iframeUrls[i];
+  listHtml += '<dt>' + iframeUrl.note + ' [' + iframeUrl.url + ']</dt><dd id="'+ iframeUrl.id + '" class="wrap"><iframe class="frame" src="'+iframeUrl.url+'" title="'+iframeUrl.note+'" width="'+iframeUrl.width+'" height="'+iframeUrl.height+'" scrolling="no"></iframe></dd>';
 }
 listHtml += "</dl>"
 var contentElem = document.getElementById("content");
